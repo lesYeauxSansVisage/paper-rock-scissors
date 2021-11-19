0b8abca2f24ac72a8a45e52eddfd7b12309163b8
Gen <genesishawkeye42@gmail.com>
2021-11-19 11:03:38 -0300

    finished the game

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 let playerScore = 0;
 let computerScore = 0;
 
+game();
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound();
+  }
+
+  const result =
+    playerScore > computerScore
+      ? `Player wins with ${playerScore} points!`
+      : `Computer wins with ${computerScore} points!`;
+
+  console.log(result);
+
+  playerScore = 0;
+  computerScore = 0;
+}
+
 function computerPlay() {
   const computerOptions = ["rock", "paper", "scissors"];
   return computerOptions[Math.floor(Math.random() * computerOptions.length)];
@@ -10,13 +28,28 @@ function playRound(computerSelection, playerSelection) {
   computerSelection = computerPlay();
   playerSelection = prompt("Please insert your choice: ").toLowerCase();
 
-  if (
+  if (playerSelection === computerSelection) {
+    console.log("That's a draw!");
+  } else if (
     (computerSelection === "scissors" && playerSelection === "paper") ||
     (computerSelection === "paper" && playerSelection === "rock") ||
-    (computerSelection === "rock" && playerSelection === "scissor")
+    (computerSelection === "rock" && playerSelection === "scissors")
   ) {
+    computerScore++;
     const captalizedChoice =
       computerSelection[0].toUpperCase() + computerSelection.slice(1);
     console.log(`Computer wins! ${captalizedChoice} beats ${playerSelection}.`);
+  } else if (
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "rock" && computerSelection === "scissors")
+  ) {
+    playerScore++;
+    const captalizedChoice =
+      playerSelection[0].toUpperCase() + playerSelection.slice(1);
+    console.log(`Player wins! ${captalizedChoice} beats ${computerSelection}.`);
+  } else {
+    alert(`It seems like there's an error!`);
   }
+  return;
 }
